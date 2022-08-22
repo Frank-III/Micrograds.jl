c52e2ece64d1eeeecd63abe0f721c8c99d3a5a9e
frankwang <1454884738@qq.com>
2022-08-22 08:55:57 -0500

    more functionality to the engine

--- src/Micrograds.jl
+++ src/Micrograds.jl
@@ -6,13 +6,13 @@ micrograd:
 =#
 #using Parameters
 
-module Micrograd
+module Micrograds
 
 import Base.@kwdef
-import Base.+, Base.*, Base.^, Base.-, Base./
+import Base.+, Base.*, Base.^, Base.-, Base./, Base.tanh
 
 
-export Value, +, -, ^, -, /, *, tanh, defaultVal
+export Value, defaultVal, backward, +, -, ^, -, /, *, tanh, relu
 
 @kwdef mutable struct Value{R<:AbstractFloat}
     data::R # for demonstration
@@ -29,12 +29,18 @@ const defaultVal = Value{Float64}
 Value{R}(d::R; label::String="", _children=Set(Value[]) ,op="") where {R <: AbstractFloat} =
     Value{R}(data=d, label=label, _prev=_children, _op = op)
 
-Value{R}(d::R, label::String, _children,op) where {R <: AbstractFloat} =
+Value{R}(d::R, label::String, _children, op) where {R <: AbstractFloat} =
     Value{R}(data=d, label=label, _prev=_children, _op = op)
 
 #show function
-
 Base.show(io::IO, val::Value) = print(io, "Value(data=$(val.data))")
+
+#some get Function
+@inline label(v::Value) = v.label
+
+@inline val(v::Value) = v.data
+
+@inline grad(v::Value) = v.grad
 
 
 
@@ -48,7 +54,7 @@ function +(v::Value, other::Value)
 end
 
 function *(v::Value, other::Value)
-    out = Value{typeof(v.data)}(data = v.data * other.data, _prev = Set([v, other]), _op = "*")
+    out = Value{typeof(v.data)}(data = v.data * other.data, _prev = Set([v, other]), _op="*")
 
     function _backward() v.grad += other.data * out.grad; other.grad += v.data * out.grad; end
     out._backward = _backward
@@ -56,19 +62,67 @@ function *(v::Value, other::Value)
     out
 end
 
+function -(v::Value, other::Value)
+    v + Value{typeof(other.data)}(-1.) * other
+end
+
+function ^(v::Value, other::Real)
+    out = Value{typeof(v.data)}(data= v.data^other, _prev= Set(Value[v]), _op="^($other)")
+
+    function _backward() v.grad += (other * v.data^(other-1)) * out.grad; end
+    out._backward = _backward
+
+    out
+end
+
+function /(v::Value, other::Value)
+    v * other^(-1)
+end
+
+function -(v::Value)
+    v * (-1)
+end
+
 function tanh(v::Value)
     x = v.data
     t = (exp(2*x) - 1) / (exp(2*x) + 1)
     out = Value{typeof(x)}(data = t, _prev = Set(Value[v]),  _op="tanh")
 
-    function _backward() v.grad = (1 - tÂ²) * out.grad; end
+    function _backward() v.grad += (1 - t^2) * out.grad; end
     out._backward = _backward
 
     out
 end
 
 function relu(v::Value)
+    out = Value{typeof(v.data)}(data = max(0, v.data), _prev = Set(Value[v]), _op = "relu")
+
+    function _backward() v.grad += (out.data > 0 ? out.grad : 0) ; end
+    out._backward = _backward
+
+    out
 end
+
+function backward(v::Value)
+    topo = Value[]; visited = Set(Value[])
+    function build_topo(v)
+        if v ∉ visited
+            push!(visited, v)
+            for child in v._prev
+                build_topo(child)
+            end
+            push!(topo, v)
+        end
+    end
+
+    build_topo(v)
+
+    v.grad = 1.
+    for v in reverse(topo)
+        v._backward()
+    end
+end
+
 
 
 end
